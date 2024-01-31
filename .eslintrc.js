8c814951405638906a2b730a7240e2fa3bcd5dfb
Alden Vallestero <alden.v@agtuary.com>
2024-01-31 13:18:59 +0800

    🚨 Align TSLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,26 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-    'extends': [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended'
-    ],
-    'overrides': [
+    "extends": "standard-with-typescript",
+    "overrides": [
         {
-            'env': {
-                'node': true
+            "env": {
+                "node": true
             },
-            'files': [
-                '.eslintrc.{js,cjs}'
+            "files": [
+                ".eslintrc.{js,cjs}"
             ],
-            'parserOptions': {
-                'sourceType': 'script'
+            "parserOptions": {
+                "sourceType": "script"
             }
         }
     ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
     },
-    'plugins': [
-        '@typescript-eslint'
-    ],
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ]
+    "rules": {
     }
 }
